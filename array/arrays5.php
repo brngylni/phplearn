68ff16f4dcfbca99aa25ec49920a6f4ad5a295cb
brngylni <barankara5@gmail.com>
2020-07-12 18:21:26 +0300

    lesson5

---
+++ array/arrays5.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title>Learning PHP</title>
+</head>
+<body>
+	<?php
+
+
+//Adding new datas to an array which created earlier than now.
+
+
+	$Values = array("Mehmet" , "Baran" , "Geylani");
+	$Values[] = "Kerim";  // You can add something after definition.
+
+	echo "<pre>";
+	print_r($Values);
+	echo "</pre>";
+
+
+
+
+
+
+
+
+
+
+	?>
+</body>
+</html>
+
+	
