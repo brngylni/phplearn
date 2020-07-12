00aadf6b02160ea985084cae64fa26ca6fe4ac69
brngylni <barankara5@gmail.com>
2020-07-12 16:43:56 +0300

    progress

--- array/arrays2.php
+++ array/arrays2.php
@@ -9,7 +9,45 @@
 <body>
 <?php
 
-fucking submodule
+/*
+
+$Names = array("Baran" , "Mehmet" , "Geylani");
+$Names =["Baran" , "Mehmet" , "Geylani"];
+
+
+$Names = array("AD1" =>  "Baran" ,  "AD2" => "Mehmet" , "SOYAD"  =>"Geylani");
+$Names = ["AD1" =>  "Baran" ,  "AD2" => "Mehmet" , "SOYAD"  =>"Geylani"];
+
+
+*/
+
+//HOW  TO CREATE AN ARRAY WİTH JUST VARİABLE STRUCTURE
+
+
+$Names[] = "Mehmet";
+$Names[] = "Baran";
+$Names[] = "Geylani";
+
+
+echo "<pre>";
+print_r($Names);
+echo "<pre/> <br/> <br/>";
+
+//We can also access to components by key name.
+
+echo $Names[0] . "<br/>";
+echo $Names[1] . "<br/>";
+echo $Names[2] . "<br/><br/><br/><br/>";
+
+//we can also determine the key names.
+
+$name["first"] = "Mehmet";
+$name["second"] = "Baran";
+
+
+echo $name["first"] . "<br/>" ;
+echo $name["second"] . "<br/>";  //calling by the key name which determined from us.
+
 
 
 ?>
