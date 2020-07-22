4205491bb27749aa82ea982eb22baf4880b9562a
brngylni <barankara5@gmail.com>
2020-07-22 14:56:50 +0300

    reset()

--- predefinedsforarrays/reset().php
+++ predefinedsforarrays/reset().php
@@ -14,9 +14,49 @@ reset() 	: We can reset the pointers location if it changed.
 We can do it with "reset()".It moves the pointer to its first location.
 */
 
+$Names 	= array("Mehmet", "Baran" , "Geylani" , "Kara" , "Kerim");
 
 
+echo "<pre>";
+print_r($Names);
+echo "</pre>";
 
+$Pointer 	= key($Names);
+echo "Pointers default key name : " . $Pointer . "<br />";
+$Element 	= current($Names);
+echo "Pointers default value : " . $Element . "<br /><br /><br />";
+
+next($Names);
+
+$Pointer2 	= key($Names);
+$Element2 	= current($Names);
+echo "Pointers changed location is " . $Pointer2 . "<br />";
+echo "Pointers changed value is " . $Element2 . "<br /><br /><br />";
+
+next($Names);
+
+$Pointer2 	= key($Names);
+$Element2 	= current($Names);
+echo "Pointers changed location is " . $Pointer2 . "<br />";
+echo "Pointers changed value is " . $Element2 . "<br /><br /><br />";
+
+
+prev($Names);
+
+$Pointer2 	= key($Names);
+$Element2 	= current($Names);
+echo "Pointers changed location is " . $Pointer2 . "<br />";
+echo "Pointers changed value is " . $Element2 . "<br /><br /><br />";
+
+
+reset($Names);
+
+$Pointer2 	= key($Names);
+$Element2 	= current($Names);
+echo "Pointers changed location is " . $Pointer2 . "<br />";
+echo "Pointers changed value is " . $Element2 . "<br /><br /><br />";
+
+echo "We can do anything to pointers location as we can see in here :)";
 
 
 
