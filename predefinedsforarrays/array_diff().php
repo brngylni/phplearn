5d8d693e5fc67f9328c6d98f5a0810a161cac497
brngylni <barankara5@gmail.com>
2020-07-26 15:01:21 +0300

    array_diff

---
+++ predefinedsforarrays/array_diff().php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title>Array_Diff</title>
+</head>
+<body>
+	<?php
+
+	/*
+	array_diff()		: It is using for create a new array from different elements in which different arrays.
+	array_diff_key()	: It is using for create a new array from different elements of different arrays which have different kay names.
+	array_diff_assoc    :It is using for create a new array from different elements of different arrays which have different kay names and element values.
+	*/
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+	?>
+</body>
+</html>
+
+	
