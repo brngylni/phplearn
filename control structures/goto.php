d563104ecddd71c0eb724dd1d4e9d7649d68c64a
Mehmet Baran Geylani <barankara5@gmail.com>
2020-08-16 17:24:53 +0300

    goto

---
+++ control structures/goto.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	goto 	:	It is using to go to the line that we targeted in the code file.
+
+	Structure :
+	goto Destination
+
+	Destination:
+	*/
+
+	goto Student; //That IM never can be a variable.
+
+	echo "Mehmet<br />"; //
+
+	echo "Baran<br />"; //
+	
+	echo "Kerim<br />"; //	These are won't work.
+
+	echo "Ali<br />"; //
+
+	Student: //That is an IM structure.
+
+	echo "Veli<br />";
+
+
+	?>
+</body>
+</html>
