55d0d90e60378bfe850b8d223eccfaad8388f51d
Mehmet Baran Geylani <barankara5@gmail.com>
2020-08-20 21:02:34 +0300

    while

---
+++ loops/while.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	/*
+
+	while 	:	It starts a loop process.
+
+	Structure :
+
+	while(conditions){
+		Code blocks
+	}
+	*/
+
+	$StartValue 	=	1;
+
+	while($StartValue <= 10){
+		echo "Loop run number:" . $StartValue . "<br />";
+		$StartValue++;
+	}
+
+	?>
+</body>
+</html>
