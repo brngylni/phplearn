959ea714d754a1fa8bf5993e03d6abdacb8d6cc5
Mehmet Baran Geylani <barankara5@gmail.com>
2020-08-20 21:11:15 +0300

    dowhile

---
+++ loops/dowhile.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+
+	do while 	:	It starts a loop process./Make a loop defination.
+		
+		Structure :
+
+		
+
+		do{
+			Code Blocks
+		}while(condition);
+	*/
+
+		$Value 	=	1;
+
+		do{
+			echo $Value . "<br />";
+			$Value++;
+		}	
+		while($Value <= 50)
+
+	?>
+</body>
+</html>
