3bcd7b18c291ba85351f2eb7372f79a429fb27d6
Mehmet Baran Geylani <barankara5@gmail.com>
2020-08-20 23:46:59 +0300

    break

---
+++ loops/break.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	/*
+	break 	:	It is using to finish loop the process
+	*/
+
+	$Start = 1;
+
+	while($Start <= 1000){
+		echo  $Start . "<br />";
+		$Start++;
+		if($Start > 40){
+			break;
+		}
+	}
+
+	?>
+</body>
+</html>
