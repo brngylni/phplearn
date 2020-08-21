67201ca96f6fc5e41be211e5f570b693dee39596
Mehmet Baran Geylani <barankara5@gmail.com>
2020-08-21 21:28:03 +0300

    functions

---
+++ functions/functions.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	Function: It is the name given to small program parts that are prepared beforehand to perform one or more specific tasks in any programming.
+	
+	Rules 	:	
+	1. Function names must always be meaningful.
+	2. Funcion names can contain alphanumeric values (a-z, A-Z, 0-9) and "_" (underscore).
+	3.Function names must always start with a letter or underscore.
+	4.Function names can not start with any number.
+	5. Funtion names can not contain space, Turkish characters or special characters.
+	6. While defining the function names, we have to care about the names/definitions that using from PHP.
+	7. Function names can never be used multiple.
+	8. Function names aren't responsive to upper-lower cases.
+	9. Range/domain rules are valid for functions.
+	
+	Structure :
+
+	function FunctionName(Parameters){
+		Code Blocks
+	}
+	*/
+
+	function Write(){
+		echo "Mehmet Geylani - Geylani Software";
+	}
+
+	Write();
+
+	?>
+</body>
+</html>
