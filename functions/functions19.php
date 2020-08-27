d1824a16fac4437aa70743af0e6e952007ad8d4d
Mehmet Baran Geylani <barankara5@gmail.com>
2020-08-27 19:13:35 +0300

    calling_with_variable_and_run_without_call

---
+++ functions/functions19.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	$Run	=	"Trial";
+
+	function Trial($Value){
+		echo $Value;
+	}
+
+	$Run("Mehmet Geylani");
+	?>
+</body>
+</html>
---
+++ functions/functions20.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	($Process	=	function(){
+		echo "Mehmet Geylani";
+	})();
+
+
+
+	?>
+</body>
+</html>
