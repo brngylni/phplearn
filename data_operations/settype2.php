479005d299811cbf24cc1b4a5b1c73fafb7e2f88
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-04 16:53:20 +0300

    settype

---
+++ data_operations/settype2.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	settype() 	:	It is using to change/define any variable content's data type.
+	*/
+
+	 $Value 		=	123;
+	 $DataType		=	gettype($Value);
+
+	 echo "The first version of content : " . $Value . "<br />";
+	 echo "The data type of first version of content : " . $DataType . "<br /><br />";
+
+	 settype($Value, "string");
+
+	 $LastDataType		=	gettype($Value);
+
+	 echo "The final version of content : " . $Value . "<br />";
+	 echo "The data type of final version of content : " . $LastDataType . "<br />";
+
+
+	?>
+</body>
+</html>
