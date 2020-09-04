e9bd4a18361e6b31b2b6ca26765353da9869991d
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-04 18:21:09 +0300

    data_type_transformations

---
+++ data_operations/transformation.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	(boolean) & (bool)				: Convert to logical data type.
+	(integer) & (int) 				: Convert to integer data type.
+	(float) & (double) & (real) 	: Convert to decimal number data type.
+	(string) 						: Convert to alphanumerical data type.
+	(array) 						: Convert to array data type.
+	(object) 						: Convert to object data type.
+	(unset)							: Convert to null data type.
+	*/
+
+	$Value1		=	"5";
+	$DataType1	=	gettype($Value1);
+
+	echo "Content of first data : " . $Value1 . "<br />";
+	echo "Data type of first data : " . $DataType1 . "<br /><br />";
+
+	$Value2		=	(integer) "5";
+	$DataType2	=	gettype($Value2);
+
+	echo "Content of first data : " . $Value2 . "<br />";
+	echo "Data type of first data : " . $DataType2 . "<br />";
+	?>
+</body>
+</html>
