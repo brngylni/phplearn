351c653e6fa0fc02e10b726e3d5939289d9f57ff
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-06 16:50:52 +0300

    appointed_multiplication

---
+++ mathematical_processes/appointed_multiplication.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	$Value		=	8;
+	echo $Value . " x 40 =";
+	$Value *= 40;
+	echo $Value; 
+	?>
+</body>
+</html>
