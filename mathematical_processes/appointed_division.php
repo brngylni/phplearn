c85a157efaea633fcc188f8fe6d984cb748fe27b
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-06 16:54:27 +0300

    appointed_division

---
+++ mathematical_processes/appointed_division.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	$Value		=	1250;
+
+	echo $Value . " / 5 = ";
+
+	$Value /= 5;
+
+	echo $Value;
+
+
+	?>
+</body>
+</html>
