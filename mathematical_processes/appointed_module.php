b2d3bd05ff41c6d9b2c269c8fd89a50b3b00ac96
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-06 17:10:18 +0300

    module

---
+++ mathematical_processes/appointed_module.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	$Value 	= 	942.321;
+
+	echo $Value . " % 13.43 = ";
+
+	$Value %= 13.43;
+
+	echo $Value;
+
+
+
+	?>
+</body>
+</html>
---
+++ mathematical_processes/module.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	$Value1 	= 	942.321;
+	$Value2		=	17.123;
+
+	$Conclusion	=	$Value1 % $Value2;
+
+	echo $Value1 . " % " . $Value2 ." = " . $Conclusion;
+
+
+
+
+	?>
+</body>
+</html>
