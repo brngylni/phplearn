c63a25574942cac7f8f2cbcac4e5fd30bd97710a
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-06 16:36:43 +0300

    appointed_extraction

---
+++ mathematical_processes/appointed_extraction.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	$Value1	=	100;
+
+	echo $Value1 . " - 23 = ";
+	
+	$Value1 -= 23;
+
+	echo $Value1;
+	?>
+</body>
+</html>
