034dfe8f7b7b2286226dc5fcfc272149c87c6785
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-06 19:36:47 +0300

    increase_and_decrease_operators

---
+++ mathematical_processes/increase.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	$Number		=	1;
+	echo $Number . "<br />";
+
+	$Number++;
+	echo $Number . "<br />";
+
+	echo ++$Number . "<br />";
+	echo $Number++ . "<br />";
+	echo $Number;
+	?>
+</body>
+</html>
