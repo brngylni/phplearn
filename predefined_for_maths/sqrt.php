130fefaa299b47d550b4a2d522780ed35551668d
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-08 01:43:40 +0300

    square_root

---
+++ predefined_for_maths/sqrt.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	sqrt()		:	It calculates square root of any value then returns it.
+	*/
+
+
+	$Number1		=	81;
+	$Number2		=	144;
+	$Number1Sqrt	=	sqrt($Number1);
+	$Number2Sqrt	=	sqrt($Number2);
+
+	echo $Number1 . " and it's square root : " . $Number1Sqrt . "<br />" ;
+	echo $Number2 . " and it's square root : " . $Number2Sqrt . "<br />" ;
+
+	?>
+</body>
+</html>
