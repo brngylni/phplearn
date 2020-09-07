67494aedceb9327d22fb0614c29d9f0d54237bbf
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-08 01:38:48 +0300

    absolute_value

---
+++ predefined_for_maths/abs.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	abs()	:	It finds absolute value of any number and returns it.
+	*/
+
+
+	$Number1		=	5;
+	$Number1Abs		=	abs($Number1);
+
+	$Number2		=	-23;
+	$Number2Abs		=	abs($Number2);
+
+	echo $Number1 . " and it's absolute value : " . $Number1Abs . "<br />" ;
+	echo $Number2 . " and it's absolute value : " . $Number2Abs . "<br />" ;
+
+	?>
+</body>
+</html>
