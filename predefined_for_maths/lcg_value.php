a2460bab2cd3c13866d794db60da32c472f0ccc1
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-07 21:12:56 +0300

    lcg_value

---
+++ predefined_for_maths/lcg_value.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	lcg_value() 	:	It returns a random decimal number which between zero and one.
+	*/
+
+	$Create		=	lcg_value();
+
+	echo "Random created number that between zero created by PHP is : " . $Create;
+
+	?>
+</body>
+</html>
