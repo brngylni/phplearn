8923ee7bb8af17d7046fcc84747c14ee7abbed15
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-08 01:45:55 +0300

    power

---
+++ predefined_for_maths/pow.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	pow()		:	It calculates the power of any number as we specified.
+	*/
+
+	$PowerNum	=	pow(5, 4);
+
+	echo "4th power of 5 is : " . $PowerNum;
+	?>
+</body>
+</html>
