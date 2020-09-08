1942262918b3c2a14dc45813979f3574bb2607c9
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-09 01:13:41 +0300

    intdiv_divident

---
+++ predefined_for_maths/intdiv.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	intdiv()	:	It returns the dividend from a division process and returns it.
+	*/
+
+	$Number1	=	2800;
+	$Number2	=	28;
+	$Divident	=	intdiv($Number1, $Number2);
+
+	echo "The divident from the division process of " . $Number1 . " and " . $Number2 . " is : " . $Divident;
+
+
+
+	?>
+</body>
+</html>
