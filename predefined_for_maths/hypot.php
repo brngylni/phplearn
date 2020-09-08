2d38144c908bd4fa9d425fc3fc4f38fe85f675b4
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-09 01:18:23 +0300

    hypotenuse

---
+++ predefined_for_maths/hypot.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	hypot() 	:	It calculates the hypotenuse of a right triangle and returns it.
+	*/
+
+	$Edge1			=	3;
+	$Edge2			=	4;
+	$Hypotenuse 	=	5;
+
+	echo "Hypotenuse of the triangle that have the " . $Edge1 . " and " . $Edge2 . " edges : " . $Hypotenuse;
+
+	?>
+</body>
+</html>
