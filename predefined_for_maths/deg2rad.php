4b5ac66a94eabf2a2a30b9b7c610a87e26cfa15d
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-09 01:27:21 +0300

    radian_and_degree_convertions

---
+++ predefined_for_maths/deg2rad.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	rad2deg()	:	It converts a degree type value to radian type value.
+	*/
+
+	$Value 			=	180;
+	$Conclusion		=	deg2rad($Value);
+
+	echo "Degree type value of " . $Value . " is : " . $Conclusion;
+
+
+	?>
+
+
+
+</body>
+</html>
---
+++ predefined_for_maths/rad2deg.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	rad2deg()	: It converts a radian type value to degree type value.
+	*/
+
+	$Value 			=	1.5;
+	$Conclusion		=	rad2deg($Value);
+
+	echo "Degree type value of " . $Value . " is : " . $Conclusion;
+
+
+
+	?>
+</body>
+</html>
