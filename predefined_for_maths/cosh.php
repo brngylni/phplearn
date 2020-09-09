627998c0e8d8e80b6e0797a0cef6a75acb327d60
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-09 20:50:40 +0300

    modification

--- predefined_for_maths/cosh.php
+++ predefined_for_maths/cosh.php
@@ -12,6 +12,10 @@
 	cosh()	:	It calculates the hyperbolic cosinus of any value and returns it.
 	*/
 
+	$Value 		=	180;
+	$HyperbolicCosinus 	=	cosh($Value);
+
+	echo "Hyperbolic cosinus of " . $Value . " is : " . $HyperbolicCosinus;
 	?>
 </body>
 </html>
