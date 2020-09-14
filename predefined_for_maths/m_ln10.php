b3a713faf6ae7cddd8e133f7cc7d4d7c900de7bb
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 19:33:39 +0300

    natural_logarithm_methods

---
+++ predefined_for_maths/m_ln10.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	M_LN2	: It returns the natural logarithm of '10' .
+	*/
+
+	$Value		=	M_LN10;
+
+	echo "Natural logarithm of 10 is : " . $Value;
+	?>
+</body>
+</html>
---
+++ predefined_for_maths/m_ln2.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	M_LN2	: It returns the natural logarithm of '2' .
+	*/
+	
+	$Value		=	M_LN2;
+
+	echo "Natural logarithm of 2 is : " . $Value;
+
+
+	?>
+</body>
+</html>
