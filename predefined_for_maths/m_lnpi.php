7467183859b40770e2f1d1ff4129390dde60b0cb
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 20:19:19 +0300

    natural_logarithm_of_PI_number

---
+++ predefined_for_maths/m_lnpi.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php	
+	/*
+	M_LNPI	:	It returns the natural logarithm of PI number.
+	*/
+
+	$Value 		=	M_LNPI;
+
+	echo "Natural logarithm of PI number is : " . $Value;
+
+	?>
+</body>
+</html>
