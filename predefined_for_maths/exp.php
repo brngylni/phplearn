5505b7c56b5ed9bced0d2b2b48ede1963e4dd6ca
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 20:39:41 +0300

    powers_of_euler_number

---
+++ predefined_for_maths/exp.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	exp()	:	It calculates the power of euler number as we specified then returns it.
+	*/
+
+	$Value 	=	exp(5);
+	$euler 	=	M_E;
+	echo "5th power of Euler Number (" . $euler . ") is : ". $Value;
+
+	?>
+</body>
+</html>
---
+++ predefined_for_maths/expm1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	expm1()	:	It calculates the power of euler number as we specified and decreases 1 then returns it..
+	*/
+
+	$Value 	=	expm1(5);
+	$euler 	=	M_E;
+	echo "1 minus of 5th power of Euler Number  (" . $euler . ") is : ". $Value;
+
+	?>
+</body>
+</html>
