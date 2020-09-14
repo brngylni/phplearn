eb47510c0e60008292e47d0e3eea162d934f637d
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 20:22:17 +0300

    euler_number_and_constant

---
+++ predefined_for_maths/m_euler.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	M_EULER		: It returns the Euler Constant	 value.
+	*/
+	
+	$Value 		=	M_EULER;
+
+	echo "The euler constant value is : " . $Value;
+	?>
+</body>
+</html>
