bfd9df844d171186ae94a0d69a981ce6ed8183d4
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 19:41:56 +0300

    logarithm_processes

---
+++ predefined_for_maths/log.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	log()	: It calculates the natural logarithm of any number then returns it.
+	*/
+
+	$number		=	8;
+	$logarithm	=	log($number);
+
+	echo "Natural logarithm of " . $number . " is : " . $logarithm;
+
+	?>
+</body>
+</html>
---
+++ predefined_for_maths/log10.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	log10()	: It calculates the base-10 natural logarithm of any number then returns it.
+	*/
+	$number		=	8;
+	$logarithm	=	log10($number);
+
+	echo "Base-10 natural logarithm of " . $number . " is : " . $logarithm;
+	?>
+</body>
+</html>
