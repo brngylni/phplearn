9ef98083908bdb0923ff003bda5704f1a1ac5415
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 19:44:41 +0300

    pi_number

---
+++ predefined_for_maths/pi.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	pi()	: It returns the pi number.
+	*/
+
+	$pinumber	=	pi();
+
+	echo "The PI number is : " . $pinumber;
+
+	?>
+</body>
+</html>
