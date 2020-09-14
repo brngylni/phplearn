dcb709287e5bdcc5e1ae3affa171a7acac85404f
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-14 20:30:34 +0300

    natural_logarithm_values_of_Euler_Number

---
+++ predefined_for_maths/m_log10e.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	M_LOG10E 	:	It returns the 10-base natural logarithm of Euler Number.
+	*/
+
+	$Value 		=	M_LOG10E;
+	$euler 		=	M_E;
+
+	echo "10-based natural logarithm of Euler Number (" . $euler . ") is : " . $Value;
+	?>
+</body>
+</html>
---
+++ predefined_for_maths/m_log2e.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	M_LOG2E 	: 	It returns the 2-base natural logarithm of Euler Number.
+	*/
+
+	$Value 		=	M_LOG2E;
+	$euler 		=	M_E;
+	echo "2-based natural logarithm of Euler Number (" . $euler . ") is : " . $Value;
+
+
+
+
+	?>
+</body>
+</html>
