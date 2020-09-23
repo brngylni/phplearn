8d9ccc92682ddd93442d0f60a1cfe1de679dcbe4
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-23 20:38:45 +0300

    preg_match()-and-preg_match_all()

---
+++ regular_expressions/preg_match.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	preg_match() 	:  It is using to controll the content according to the parameter that consists regular expressions that we specified then it's creates.If it'd catch a match, it creates an array and return it.
+
+	preg_match_all() 	:  It is using to controll the content according to the parameter that consists regular expressions that we specified then it's creates.If it'd catch a match, it creates an array and return it.It returns the all matchs.
+	*/
+	$Text 		=	"Hello , my name is Mehmet Geylani, ı am a php developer.You can find me on facebook by the search 'Mehmet Geylani'";
+	$Pattern 	=	"/Mehmet/";
+
+	preg_match($Pattern, $Text, $Result);
+
+	echo "<pre>";
+	print_r($Result);
+	echo "</pre>";
+
+	if($Result){
+		echo "There is a match!";
+	}else{
+		echo "There isn't any matches";
+	}
+
+	?>
+</body>
+</html>
