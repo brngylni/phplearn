e63bfd250286be6b9a1ac7903010eb101ee053bb
Mehmet Baran Geylani <barankara5@gmail.com>
2020-09-25 00:29:12 +0300

    x_parameter_(ignoring_space_in_the_pattern)

---
+++ regular_expressions/x_parameter.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	x 	:  It is using to control any content by ignoring the space in the pattern.	IT ONLY IGNORES THE SPACE IN THE PATTERN, CAN'T DO ANYTHİNG TO SPACE IN THE CONTENT.
+	*/
+
+	$Text 		=	"Mehmet Geylani, PHP Student.";
+	$Pattern 	=	"/P H P/x"; //It can't cath any match without 'x' parameter.
+	preg_match($Pattern, $Text, $Result);
+
+	echo "The original content : " . $Text . "<br />";
+	echo "The pattern : " . $Pattern . "<br />";
+	echo "The new content : <br /><pre>";
+	print_r($Result);
+	echo "</pre>";
+
+
+
+	?>
+</body>
+</html>
