1c7be830591cd169dfccf422e9753753e8060e79
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-15 15:49:18 +0300

    touch

---
+++ file_operations/touch.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	touch() 	: It is using to create a new file according to specified value.Also additionally returns the process result's boolean value.
+	*/
+
+	$file 		= 	"folder/file2.txt";
+	touch($file);
+		
+	?>
+</body>
+</html>
