598ccb60de12c66585aadb13a81aff2821ff744c
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-17 21:10:05 +0300

    rmdir

---
+++ file_operations/rmdir.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	rmdir() 	:	It is using to remove a folder.It can also return the result as a boolean value.
+	*/
+
+	$folder 	=	"folder/Images";
+	rmdir($folder);
+
+	?>
+</body>
+</html>
