c2cef79e3f3b9eabeeb071583376ec787121baf3
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-17 21:56:20 +0300

    GLOB_ERR

---
+++ file_operations/glob9.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+		$folder 	=	"D:/xampp/htdocs/phplearn/*";
+		
+		$search 	=	glob($folder , GLOB_ERR); // Stop when an error occurs:
+		echo "<pre>";
+		print_r($search);
+		echo "</pre>";
+
+	?>
+</body>
+</html>
