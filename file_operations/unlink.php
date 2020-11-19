e027f07db4b597ce0e584e4a02f3d76a8d1c2844
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-19 12:26:03 +0300

    unlink

---
+++ file_operations/unlink.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	unlink() 	: It is using to delete files.It can also return the result as a boolean value.
+	*/
+	$file 	=	"folder/file2.txt";
+	unlink($file); // Deleted the file2.txt
+
+	?>
+</body>
+</html>
