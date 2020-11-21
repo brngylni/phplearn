f2167d6178ff5a2631dd524c54a7a6200ad64fd6
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-21 20:09:00 +0300

    fwrite()

---
+++ file_operations/fwrite.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	fwrite() 	:	It is using to write any content to specified file.It can also return how many characters writed .
+	*/
+
+	$content 		=	"Mehmet Geylani plays guitar.\n";
+	$file 			=	"folder/file.txt";
+	$fileOpen		=	fopen($file, "a");
+	$charNum 		=	0;
+	
+	$totalCharNum	=	0;
+
+	for($i = 0; $i< 100; $i++){
+	$charNum		+=	fwrite($fileOpen, $content);
+	}
+	fclose($fileOpen);
+	$fileOpen 	=	fopen($file, "r");
+
+	while(!feof($fileOpen)){
+		$chars			=	fgetc($fileOpen);
+		$totalCharNum++;
+	}
+
+	echo "Added character number : " . $charNum . "<br />Total Character number : " . $totalCharNum;
+
+	fclose($fileOpen);
+	?>
+</body>
+</html>
