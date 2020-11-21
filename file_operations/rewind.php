e18d77a14cfbd96a4886572570df2aa27154c6c1
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-21 16:31:59 +0300

    rewind

---
+++ file_operations/rewind.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	fseel() 	:	It is using to reset the changed location that the file will be read from.
+	*/
+
+	$file 		=	"D:/xampp/htdocs/phplearn/file_operations/folder/file.txt";
+	$fileOpen 	=	fopen($file, "r");
+
+	$location 	=	ftell($fileOpen);
+
+	echo "Location : " . $location . "<br />";
+	fseek($fileOpen, 7);
+	$location 	=	ftell($fileOpen);
+	echo "Location : " . $location . "<br />";
+	rewind($fileOpen);
+	$location 	=	ftell($fileOpen);
+	echo "Location : " . $location . "<br />";
+	$read 	=	fpassthru($fileOpen);
+
+	echo $read;
+	fclose($fileOpen);
+
+	?>
+</body>
+</html>
