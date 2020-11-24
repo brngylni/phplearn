11aa2c126d6ad003fa4b9012fd6ee792ae795981
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-24 19:34:36 +0300

    cookies_and_sessions

---
+++ cookies_and_sessions/setcookie.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*	
+	setcookie() 	: It is using to create or delete cookies.
+	Parameters 	:	
+
+	1. Cookie Name
+	2. Cookie Value
+	3. Cookie Lifetime
+	*/
+
+	$lifetime 	=	time() + (60 * 60);
+	setcookie("Username", "Baran", $lifetime);
+	setcookie("UsernSurname", "Geylani", $lifetime);
+	setcookie("School", "Adu", $lifetime);
+
+	echo "<pre>";
+	print_r($_COOKIE); // We can access to cookies with that super global.
+	echo "</pre>";
+
+	?>
+</body>
+</html>
