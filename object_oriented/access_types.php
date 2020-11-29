8f403b1e12c75441c7f63b4ff384adaa61dfac2e
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-29 13:20:43 +0300

    definitions_of_accession_types

---
+++ object_oriented/access_types.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	static 		:	Constant. Can be accessed unless calling class.
+	
+	$this 		:	Using to mark the class like this class.
+	
+	self:: 		:	Using to mark the class like self his/him/it's class.
+
+	parent:: 	:	Using to mark the class like parent's class.
+	*/
+
+	?>
+</body>
+</html>
