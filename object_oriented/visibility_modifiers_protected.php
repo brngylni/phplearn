eda496eb19200987211347b6366119da775be927
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-29 13:22:52 +0300

    protected_modifier_example

---
+++ object_oriented/visibility_modifiers_protected.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	class One{
+
+		protected $name 	= "Mehmet";
+		protected const SURNAME = "Geylani";	// It will print 1 error because in classes, any error will stop the ru
+
+		protected function information(){
+			$text 	=	"is a CS student.";
+			return $text;
+		}
+	}
+
+	$object 	=	new One;
+
+	echo $object->name . " " . One::SURNAME . " " . $object->information();
+
+	?>
+
+
+</body>
+</html>
