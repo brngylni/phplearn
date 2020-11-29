ba2e9ea1ee75c5554443351a620c60f149e726c9
Mehmet Baran Geylani <barankara5@gmail.com>
2020-11-29 13:23:19 +0300

    public_modifier_example

---
+++ object_oriented/visibility_modifiers_public.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+
+	class One{
+
+		public $name 	= "Mehmet";
+		public const SURNAME = "Geylani";	// It will print 1 error because in classes, any error will stop the ru
+
+		public function information(){
+			$text 	= $this->name . " " . self::SURNAME . " is a CS student."; 	// We have to use $this and self:: keywords to access to properties/data field.
+			return $text;
+		}
+	}
+
+	$object 	=	new One;
+
+	echo $object->information();
+	?>
+
+
+</body>
+</html>
