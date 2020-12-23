c06cfe60b41262fb7e9707062995fb81d619c1fb
Mehmet Baran Geylani <barankara5@gmail.com>
2020-12-23 21:10:58 +0300

    interfaces

---
+++ object_oriented/interfaces.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	interface Trial{
+		public function define($parameterContent);
+	}
+	class Test implements Trial{
+		public function define($parameterContent){
+
+		}
+	}
+
+	?>
+</body>
+</html>
