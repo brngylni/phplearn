f2dc637e70d2b33a7e668e274fd2244b0fdff6d2
Mehmet Baran Geylani <barankara5@gmail.com>
2020-12-30 22:36:13 +0300

    spl_autoload_register()

---
+++ object_oriented/spl.php
@@ -0,0 +1,12 @@
+<?php
+	class spl{
+		public $name = "Mehmet";
+		public $surname = "Geylani";
+
+		public function informations(){
+			return $this->name . " " . $this->surname;
+
+		} 
+	}
+
+?>
---
+++ object_oriented/spl_autoload_register.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	spl_autoload_register() : It is using to 
+	*/
+	function autoRun($value){
+		$fileName = $value . ".php";
+		require_once($fileName);
+	}
+
+	spl_autoload_register("autoRun");
+	$who = new spl;
+	echo $who->informations();
+
+	?>
+</body>
+</html>
