31acb8ac7d789a78f7b61b01df6ac87737c0e9eb
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-03 22:41:43 +0300

    is_readable

---
+++ control_methods/is_readable.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	is_readable() : It is using to check either the specified file is readable or not.
+	*/
+
+	$file = "sample2.txt";
+
+	if(is_readable($file)){
+		echo "The file is readable";
+	}else{
+		echo "The file isn't readable";
+	}
+	?>
+</body>
+</html>
