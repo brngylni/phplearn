8994829982602b9e9fb76cbd6d8a8328b970fede
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-04 19:08:45 +0300

    printf

---
+++ various_operations/printf.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	printf() : It is using to print some content according to parameters.
+	*/
+
+	$data = "Mehmet Geylani";
+
+	printf("Welcome Mr. %s ", $data);
+
+	?>
+</body>
+</html>
