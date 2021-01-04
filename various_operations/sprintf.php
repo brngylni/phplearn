81aa9bf33c677ca0ef7bea449cfa394f916a16aa
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-04 19:08:54 +0300

    sprintf

---
+++ various_operations/sprintf.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	sprintf() : It is using to memorize some content to a variable according to parameters.
+	*/
+
+	$data = sprintf("Mehmet Geylani");
+
+	printf($data);	
+
+	?>
+</body>
+</html>
