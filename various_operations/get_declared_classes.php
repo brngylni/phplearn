e07eeb10ade268e30b4b34c03c48a21d0bcf2e34
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-05 15:22:59 +0300

    get_declared_classes

---
+++ various_operations/get_declared_classes.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	get_declared_classes() : It finds the list of whole classes in PHP and defined by user then returns those as an array.
+	*/
+	class Trial{
+
+	}
+
+	echo "<pre>";
+	print_r(get_declared_classes());
+	echo "</pre>";
+
+	?>
+</body>
+</html>
