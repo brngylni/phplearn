7270289ba91f27dc2334ddc1765ac9c46e09675b
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-05 17:14:30 +0300

    php_ini_loaded_file

---
+++ various_operations/php_ini_loaded_file.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	php_ini_loaded_file() : 	It is using to path of php.ini
+	*/
+
+	echo "path of php.ini file : " . php_ini_loaded_file();
+
+	?>
+</body>
+</html>
