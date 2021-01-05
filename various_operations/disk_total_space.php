ad6347296bbfc5e928f44b65b619190919dd1d97
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-05 23:34:23 +0300

    disk_total_space

---
+++ various_operations/disk_total_space.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	disk_total_space() : It returns the total disk space.
+	*/
+
+	echo "Total disk space is " . disk_total_space("D:") . " byte.";
+	?>
+</body>
+</html>
