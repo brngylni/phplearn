c17c19655775d002a9e86ceb733900006d14135f
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-05 23:34:09 +0300

    disk_free_space

---
+++ various_operations/disk_free_space.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/* 
+	disk_free_space() : It returns the usable disk space.
+	*/
+
+	echo "This system has " . disk_free_space("D:") . " byte of free space."
+
+	?>
+</body>
+</html>
