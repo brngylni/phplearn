a18a9a0894d397c7a7a10a97572024a5838590af
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-06 22:26:51 +0300

    sys_get_temp_dir

---
+++ various_operations/sys_get_temp_dir.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	sys_get_temp_dir() : It returns the current temporary directory.
+	*/
+
+	echo sys_get_temp_dir();
+
+	?>
+</body>
+</html>
