c92a948d5ff564a86960f2be77b4e47443432b12
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-06 22:27:00 +0300

    zend_version

---
+++ various_operations/zend_version.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	zend_version() : It returns the zend engine version.
+	*/
+
+	echo zend_version();
+
+	?>
+</body>
+</html>
