fd4ae11da2f336c61eaa269cf2032bf7450975e2
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-08 22:35:49 +0300

    sleep

---
+++ various_operations/sleep.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	sleep() : It delays the codes that after the line that sleep used for specified time.
+	*/
+
+	echo "Mehmet Geylani<br />";
+	sleep(5); // It delays the process 5 seconds.
+	echo "Baran Geylani";
+
+	?>
+</body>
+</html>
