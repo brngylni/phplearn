bb2619aaea81c8acb5c565a2e8477713dd0bade8
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-20 12:25:00 +0300

    exit_die

---
+++ various_operations/exit.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+	/*
+	exit() 	: It is using to kill the process. It can also return the specified value.
+	*/
+
+	echo "Baran";
+	exit();
+	echo "Geylani";
+
+
+	?>
+</body>
+</html>
