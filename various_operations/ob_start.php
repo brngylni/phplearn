7f7bcf2a0d98809de9b723373ee78787a068fd18
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-20 12:54:55 +0300

    ob_start

---
+++ various_operations/ob_start.php
@@ -0,0 +1,20 @@
+<?php
+session_start();	ob_start();
+?>
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+		/*
+		ob_start() 	: It starts the output buffer. It must be used on top of the page.
+		*/
+
+	?>
+</body>
+</html>
