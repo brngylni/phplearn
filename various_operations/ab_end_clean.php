782e41db09eb48fa8e6833a5de7d4011d1ac35d6
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-20 12:55:14 +0300

    ob_end_clean

---
+++ various_operations/ab_end_clean.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+		/*
+		ob_end_clean() 	: It deletes the output buffer. 
+		*/
+	?>
+</body>
+</html>
+<?php
+	ob_end_clean()
+?>
