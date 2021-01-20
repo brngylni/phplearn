c7302f69027db018cc670334b4731d28c816e612
Mehmet Baran Geylani <barankara5@gmail.com>
2021-01-20 12:55:26 +0300

    ob_end_flush

---
+++ various_operations/ob_end_flush.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+	<?php
+		/*
+		ob_end_flush() 	: It cleans the output buffer then stops it. 
+		*/
+	?>
+</body>
+</html>
+<?php
+	ob_end_flush()
+?>
